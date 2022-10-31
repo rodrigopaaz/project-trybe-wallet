991f40df76f6df2527dae2decb9c9d4eef6f3ad4
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-10-31 16:56:12 -0300

    01 - realizado o primeiro requisito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addUser } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  validMail = (value) => {
+    const check = /\S+@\S+\.\S+/;
+    return check.test(value);
+  };
+
   render() {
-    return <div>Login</div>;
+    const { history, dispatch } = this.props;
+    const { email, password } = this.state;
+    return (
+      (
+        <div>
+          Login
+          <div>
+            <label htmlFor="email">
+              Email
+              <input
+                type="text"
+                data-testid="email-input"
+                name="email"
+                onChange={ this.handleChange }
+
+              />
+            </label>
+            <label htmlFor="password">
+              Senha
+              <input
+                type="password"
+                data-testid="password-input"
+                name="password"
+                onChange={ this.handleChange }
+
+              />
+            </label>
+            <button
+              type="button"
+              disabled={
+                !(this.validMail(email) && password.length >= Number('6'))
+              }
+              onClick={ () => {
+                history.push('/carteira');
+                dispatch(addUser(email));
+              } }
+            >
+              Entrar
+            </button>
+          </div>
+        </div>
+      )
+    );
   }
 }
 
-export default Login;
+Login.propTypes = ({
+  history: PropTypes.string,
+  push: PropTypes.string,
+}).isRequired;
+
+export default connect()(Login);
