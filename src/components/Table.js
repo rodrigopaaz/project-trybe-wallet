955a80fb279a7d43355906194484186f149cf410
rodrigo <rodrigo_pazlimas@hotmail.com>
2022-11-02 12:42:04 -0300

    07 - realizado o requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,73 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { wallet } = this.props;
+    const { expenses } = wallet;
+
+    console.log(wallet);
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            { expenses.map((element, index) => (
+              <tr key={ index }>
+                <td>{element.description}</td>
+                <td>{element.tag}</td>
+                <td>{element.method}</td>
+                <td>{(Number(element.value)).toFixed(2)}</td>
+                <td>{element.exchangeRates[element.currency].name}</td>
+                <td>
+                  {(Number(element.exchangeRates[element.currency].ask))
+                    .toFixed(2)}
+
+                </td>
+                <td>
+                  {(element.value * [element.exchangeRates[element.currency].ask])
+                    .toFixed(2)}
+
+                </td>
+                <td>Real</td>
+                <td>
+                  <button
+                    type="button"
+                    onClick={ () => {} }
+                  >
+                    Editar/Excluir
+                  </button>
+
+                </td>
+              </tr>))}
+          </tbody>
+
+        </table>
+      </div>
     );
   }
 }
 
-export default connect()(Table);
+Table.propTypes = ({
+  wallet: PropTypes.shape,
+  expenses: PropTypes.shape,
+}).isRequired;
+
+const mapStateToProps = (state) => ({
+  wallet: state.wallet,
+});
+
+export default connect(mapStateToProps)(Table);
