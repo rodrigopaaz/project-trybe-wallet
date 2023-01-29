66bb9fafcac6af5df0f82371e194fa1b1065c3d0
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-01-29 15:14:02 -0300

    FEAT:DEPLOY

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -49,66 +49,64 @@ class WalletForm extends Component {
     return (
       <div>
         <div className="div__wallet">
-          <div className="div__wallet__inputs">
-            <label htmlFor="description" className="label__top">
-              Descrição da despesa
-              <input
-                data-testid="description-input"
-                name="description"
-                onChange={ this.handleChange }
-                value={ description }
-              />
-            </label>
-            <label htmlFor="tag" className="label__top">
-              Categoria da despesa
-              <select
-                data-testid="tag-input"
-                name="tag"
-                value={ tag }
-                onChange={ this.handleChange }
-              >
-                <option>Alimentação</option>
-                <option>Lazer</option>
-                <option>Trabalho</option>
-                <option>Transporte</option>
-                <option>Saúde</option>
-              </select>
-            </label>
-            <label htmlFor="value" className="label__botton">
-              Valor
-              <input
-                data-testid="value-input"
-                name="value"
-                type="number"
-                onChange={ this.handleChange }
-                value={ (value) }
-              />
-            </label>
-            <label htmlFor="method" className="label__botton">
-              Método de pagamento
-              <select
-                data-testid="method-input"
-                name="method"
-                value={ method }
-                onChange={ this.handleChange }
-              >
-                <option>Dinheiro</option>
-                <option>Cartão de crédito</option>
-                <option>Cartão de débito</option>
-              </select>
-            </label>
-            <label htmlFor="currency" className="label__botton">
-              Moeda
-              <select
-                data-testid="currency-input"
-                onChange={ this.handleChange }
-                name="currency"
-                value={ currency }
-              >
-                {currencies.map((element, key) => <option key={ key }>{element}</option>)}
-              </select>
-            </label>
-          </div>
+          <label htmlFor="description" className="label__top">
+            Descrição da despesa
+            <input
+              data-testid="description-input"
+              name="description"
+              onChange={ this.handleChange }
+              value={ description }
+            />
+          </label>
+          <label htmlFor="tag" className="label__top">
+            Categoria da despesa
+            <select
+              data-testid="tag-input"
+              name="tag"
+              value={ tag }
+              onChange={ this.handleChange }
+            >
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+          <label htmlFor="value" className="label__botton">
+            Valor
+            <input
+              data-testid="value-input"
+              name="value"
+              type="number"
+              onChange={ this.handleChange }
+              value={ (value) }
+            />
+          </label>
+          <label htmlFor="method" className="label__botton">
+            Método de pagamento
+            <select
+              data-testid="method-input"
+              name="method"
+              value={ method }
+              onChange={ this.handleChange }
+            >
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="currency" className="label__botton">
+            Moeda
+            <select
+              data-testid="currency-input"
+              onChange={ this.handleChange }
+              name="currency"
+              value={ currency }
+            >
+              {currencies.map((element, key) => <option key={ key }>{element}</option>)}
+            </select>
+          </label>
         </div>
         <div className="div__button">
           { !editMode
